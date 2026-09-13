d083dbbbbbfd83799101375de74cca0a1ee057ce
anuradha55 <anu.gujar35@gmail.com>
2026-09-13 12:36:55 +0530

    Set Hindi as default document language

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="hi">
       <body>
         {children}
         <TimePickerEnhancer />
